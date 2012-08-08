4f5d765305914ce33c93d3ed487c3dfa7b1b98b1
Lin Lin--Colosseum <linlin@lbl.gov>
2012-08-07 22:50:01 -0700

    small change

--- UTILITIES/readmat.m
+++ UTILITIES/readmat.m
@@ -5,9 +5,10 @@ sizeTriplet = fread(fh, 1, 'int');
 RowVec = fread(fh, sizeTriplet, 'int');
 ColVec = fread(fh, sizeTriplet, 'int');
 ValVec = fread(fh, sizeTriplet, 'double');
+disp('Finish reading');
 
 A = sparse(RowVec+1, ColVec+1, ValVec, sizeA, sizeA);
-D = eig(full(A));
+D = eigs(A+10*speye(size(A)),6,'sm')-10;
 
 fclose(fh);
 
