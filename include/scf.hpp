dbe7bc0a9dca2d01dba743020acfc32690b18cae
Lin Lin--Colosseum <linlin@lbl.gov>
2012-10-17 23:06:36 -0700

    SCF outputs eigenvalue

---
+++ include/scf.hpp
@@ -0,0 +1,107 @@
+#ifndef _SCF_HPP_ 
+#define _SCF_HPP_
+
+#include  "environment_impl.hpp"
+#include  "numvec_impl.hpp"
+#include  "domain.hpp"
+#include  "fourier.hpp"
+#include  "hamiltonian.hpp"
+#include  "spinor.hpp"
+#include  "periodtable.hpp"
+#include  "esdf.hpp"
+#include  "eigensolver.hpp"
+#include  "utility.hpp"
+#include	"blas.hpp"
+#include	"lapack.hpp"
+
+// TODO In the case of K-point, should the occupation rate, the energy
+// and the potential subroutines be moved to SCF class rather than
+// stay in the Hamiltonian class?
+
+namespace dgdft{
+
+class SCF
+{
+private:
+	// Control parameters
+	Int                 mixMaxDim_;
+	std::string         mixType_;
+	Real                mixStepLength_;            
+	Real                scfTolerance_;
+	Int                 scfMaxIter_;
+	bool                isRestartDensity_;
+	bool                isRestartWfn_;
+	bool                isOutputDensity_;
+	bool                isOutputWfn_;
+  
+	std::string         restartDensityFileName_;
+  std::string         restartWfnFileName_;
+
+	// Physical parameters
+	Real                Tbeta_;                    // Inverse of temperature in atomic unit
+	Real                Efree_;                    // Helmholtz free energy
+	Real                Etot_;                     // Total energy
+	Real                Ekin_;                     // Kinetic energy
+	Real                Ecor_;                     // Nonlinear correction energy
+	Real                Exc_;                      // Exchange-correlation energy
+	Real                fermi_;                    // Fermi energy
+
+	Real                totalCharge_;              // Total number of computed electron charge
+	
+	EigenSolver*        eigSolPtr_;
+	PeriodTable*        ptablePtr_;
+
+	// SCF variables
+  DblNumVec           vtotNew_;
+	Real                scfNorm_;                 // ||V_{new} - V_{old}|| / ||V_{old}||
+	// for Anderson iteration
+	DblNumMat           dfMat_;
+	DblNumMat           dvMat_;
+	// TODO Elliptic preconditioner
+
+public:
+	
+	
+	// *********************************************************************
+	// Life-cycle
+	// *********************************************************************
+	SCF();
+	~SCF();
+  
+	// *********************************************************************
+	// Operations
+	// *********************************************************************
+	// Basic parameters. Density and wavefunction
+	void  Setup( const esdf::ESDFInputParam& esdfParam, EigenSolver& eigSol, PeriodTable& ptable ); 
+	void  Iterate();
+
+	void  CalculateOccupationRate ( DblNumVec& eigVal, DblNumVec& occupationRate );
+	void  CalculateEnergy();
+	void  PrintState( const Int iter );
+
+	// Mixing
+	// TODO
+	void  AndersonMix( const Int iter );
+	void  KerkerMix();
+	// TODO
+//	void  EllipticMix();
+
+	// *********************************************************************
+	// Inquiry
+	// *********************************************************************
+	// Energy etc.
+	
+
+}; // -----  end of class  SCF ----- 
+
+
+
+// *********************************************************************
+// Other subroutines
+// *********************************************************************
+void PrintInitialState( const esdf::ESDFInputParam& esdfParam );
+
+
+} // namespace dgdft
+#endif // _SCF_HPP_
+
---
+++ src/scf.cpp
@@ -0,0 +1,627 @@
+#include  "scf.hpp"
+
+namespace  dgdft{
+
+SCF::SCF	(  )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::SCF");
+#endif
+	eigSolPtr_ = NULL;
+	ptablePtr_ = NULL;
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+} 		// -----  end of method SCF::SCF  ----- 
+
+SCF::~SCF	(  )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::~SCF");
+#endif
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+} 		// -----  end of method SCF::~SCF  ----- 
+
+
+void
+SCF::Setup	( const esdf::ESDFInputParam& esdfParam, EigenSolver& eigSol, PeriodTable& ptable )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::Setup");
+#endif
+	
+	// esdf parameters
+	{
+    mixMaxDim_     = esdfParam.mixMaxDim;
+    mixType_       = esdfParam.mixType;
+		mixStepLength_ = esdfParam.mixStepLength;
+		scfTolerance_  = esdfParam.scfTolerance;
+		scfMaxIter_    = esdfParam.scfMaxIter;
+		isRestartDensity_ = esdfParam.isRestartDensity;
+		isRestartWfn_     = esdfParam.isRestartWfn;
+    Tbeta_         = esdfParam.Tbeta;
+	}
+
+	// other SCF parameters
+	{
+		eigSolPtr_ = &eigSol;
+    ptablePtr_ = &ptable;
+
+		Int ntot = eigSolPtr_->Psi().NumGridTotal();
+
+		vtotNew_.Resize(ntot); SetValue(vtotNew_, 0.0);
+		dfMat_.Resize( ntot, mixMaxDim_ ); SetValue( dfMat_, 0.0 );
+		dvMat_.Resize( ntot, mixMaxDim_ ); SetValue( dvMat_, 0.0 );
+	
+		restartDensityFileName_ = "DEN";
+		restartWfnFileName_     = "WFN";
+	}
+
+	// Density
+	{
+    DblNumMat&  density = eigSolPtr_->Ham().Density();
+		if( isRestartDensity_ ) {
+			std::istringstream rhoStream;      
+			SharedRead( restartDensityFileName_, rhoStream);
+			// TODO Error checking
+			deserialize( density, rhoStream, NO_MASK );    
+		} // else using the zero initial guess
+		else {
+			// make sure the pseudocharge is initialized
+			DblNumVec&  pseudoCharge = eigSolPtr_->Ham().PseudoCharge();
+			
+			SetValue( density, 0.0 );
+      
+			Int ntot = eigSolPtr_->Psi().NumGridTotal();
+			Real sum0 = 0.0, sum1 = 0.0;
+			Real EPS = 1e-6;
+
+			// make sure that the electron density is positive
+			for (Int i=0; i<ntot; i++){
+				density(i, VAL) = ( pseudoCharge(i) > EPS ) ? pseudoCharge(i) : EPS;
+				sum0 += density(i, VAL);
+				sum1 += pseudoCharge(i);
+			}
+			
+			// Rescale the density
+			for (int i=0; i <ntot; i++){
+				density(i, VAL) *= sum1 / sum0;
+			} 
+		}
+	}
+	if( !isRestartWfn_ ) {
+		UniformRandom( eigSolPtr_->Psi().Wavefun() );
+	}
+	else {
+		std::istringstream iss;
+		SharedRead( restartWfnFileName_, iss );
+		deserialize( eigSolPtr_->Psi().Wavefun(), iss, NO_MASK );
+	}
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::Setup  ----- 
+
+
+void
+SCF::Iterate	(  )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::Iterate");
+#endif
+
+#ifndef _RELEASE_
+	PushCallStack("SCF::Iterate::Initialize");
+#endif
+
+	// Compute the exchange-correlation potential and energy
+	eigSolPtr_->Ham().CalculateXC( Exc_ ); 
+
+	// Compute the Hartree energy
+	eigSolPtr_->Ham().CalculateHartree( eigSolPtr_->FFT() );
+	// No external potential
+
+	// Compute the total potential
+	eigSolPtr_->Ham().CalculateVtot( eigSolPtr_->Ham().Vtot() );
+
+
+  Real timeIterStart(0), timeIterEnd(0);
+  
+	bool isSCFConverged = false;
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+
+  for (Int iter=1; iter <= scfMaxIter_; iter++) {
+		
+		// *********************************************************************
+		// Performing each iteartion
+		// *********************************************************************
+    
+    GetTime( timeIterStart );
+
+    if ( isSCFConverged ) break;
+
+		// Solve the eigenvalue problem
+		eigSolPtr_->Solve();
+		// No need for normalization using LOBPCG
+
+		// Compute the occupation rate
+		CalculateOccupationRate( eigSolPtr_->Ham().EigVal(), 
+				eigSolPtr_->Ham().OccupationRate() );
+
+		// Compute the electron density
+		eigSolPtr_->Ham().CalculateDensity(
+				eigSolPtr_->Psi(),
+				eigSolPtr_->Ham().OccupationRate(),
+        totalCharge_);
+
+		// Compute the exchange-correlation potential and energy
+		eigSolPtr_->Ham().CalculateXC( Exc_ ); 
+		
+		// Compute the Hartree energy
+		eigSolPtr_->Ham().CalculateHartree( eigSolPtr_->FFT() );
+		// No external potential
+		
+		// Compute the total potential
+		eigSolPtr_->Ham().CalculateVtot( vtotNew_ );
+
+		Real normVtotDif = 0.0, normVtotOld;
+		DblNumVec& vtotOld_ = eigSolPtr_->Ham().Vtot();
+		Int ntot = vtotOld_.m();
+		for( Int i = 0; i < ntot; i++ ){
+			normVtotDif += pow( vtotOld_(i) - vtotNew_(i), 2.0 );
+			normVtotOld += pow( vtotOld_(i), 2.0 );
+		}
+		normVtotDif = sqrt( normVtotDif );
+		normVtotOld = sqrt( normVtotOld );
+		scfNorm_    = normVtotDif / normVtotOld;
+		
+    CalculateEnergy();
+
+    PrintState( iter );
+
+
+    if( scfNorm_ < scfTolerance_ ){
+      /* converged */
+      Print( statusOFS, "SCF is converged!\n" );
+      isSCFConverged = true;
+    }
+
+		// Potential mixing
+    if( mixType_ == "anderson" ){
+      AndersonMix(iter);
+    }
+    if( mixType_ == "kerker" ){
+      KerkerMix();  
+      AndersonMix(iter);
+    }
+
+		GetTime( timeIterEnd );
+   
+		Print( statusOFS, "Wall clock time = ", timeIterEnd - timeIterStart );
+  }
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::Iterate  ----- 
+
+void
+SCF::CalculateOccupationRate	( DblNumVec& eigVal, DblNumVec& occupationRate )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::CalculateOccupationRate");
+#endif
+	// For a given finite temperature, update the occupation number */
+	// FIXME Magic number here
+	Real tol = 1e-10; 
+	Int maxiter = 100;  
+
+	Real lb, ub, flb, fub, occsum;
+	Int ilb, iub, iter;
+
+	Int npsi       = eigSolPtr_->Ham().NumStateTotal();
+	Int nOccStates = eigSolPtr_->Ham().NumOccupiedState();
+
+	if( eigVal.m() != npsi ){
+		std::ostringstream msg;
+		msg 
+			<< "The number of eigenstates do not match."  << std::endl
+			<< "eigVal         ~ " << eigVal.m() << std::endl
+			<< "numStateTotal  ~ " << npsi << std::endl;
+		throw std::logic_error( msg.str().c_str() );
+	}
+
+
+	occupationRate.Resize( npsi );
+
+	if( npsi > nOccStates )  {
+		/* use bisection to find efermi such that 
+		 * sum_i fermidirac(ev(i)) = nocc
+		 */
+		ilb = nOccStates-1;
+		iub = nOccStates+1;
+
+		lb = eigVal(ilb-1);
+		ub = eigVal(iub-1);
+
+		/* Calculate Fermi-Dirac function and make sure that
+		 * flb < nocc and fub > nocc
+		 */
+
+		flb = 0.0;
+		fub = 0.0;
+		for(Int j = 0; j < npsi; j++) {
+			flb += 1.0 / (1.0 + exp(Tbeta_*(eigVal(j)-lb)));
+			fub += 1.0 / (1.0 + exp(Tbeta_*(eigVal(j)-ub))); 
+		}
+
+		while( (nOccStates-flb)*(fub-nOccStates) < 0 ) {
+			if( flb > nOccStates ) {
+				if(ilb > 0){
+					ilb--;
+					lb = eigVal(ilb-1);
+					flb = 0.0;
+					for(Int j = 0; j < npsi; j++) flb += 1.0 / (1.0 + exp(Tbeta_*(eigVal(j)-lb)));
+				}
+				else {
+					throw std::logic_error( "Cannot find a lower bound for efermi" );
+				}
+			}
+
+			if( fub < nOccStates ) {
+				if( iub < npsi ) {
+					iub++;
+					ub = eigVal(iub-1);
+					fub = 0.0;
+					for(Int j = 0; j < npsi; j++) fub += 1.0 / (1.0 + exp(Tbeta_*(eigVal(j)-ub)));
+				}
+				else {
+					throw std::logic_error( "Cannot find a lower bound for efermi, try to increase the number of wavefunctions" );
+				}
+			}
+		}  /* end while */
+
+		fermi_ = (lb+ub)*0.5;
+		occsum = 0.0;
+		for(Int j = 0; j < npsi; j++) {
+			occupationRate(j) = 1.0 / (1.0 + exp(Tbeta_*(eigVal(j) - fermi_)));
+			occsum += occupationRate(j);
+		}
+
+		/* Start bisection iteration */
+		iter = 1;
+		while( (fabs(occsum - nOccStates) > tol) && (iter < maxiter) ) {
+			if( occsum < nOccStates ) {lb = fermi_;}
+			else {ub = fermi_;}
+
+			fermi_ = (lb+ub)*0.5;
+			occsum = 0.0;
+			for(Int j = 0; j < npsi; j++) {
+				occupationRate(j) = 1.0 / (1.0 + exp(Tbeta_*(eigVal(j) - fermi_)));
+				occsum += occupationRate(j);
+			}
+			iter++;
+		}
+	}
+	else {
+		if (npsi == nOccStates ) {
+			for(Int j = 0; j < npsi; j++) 
+				occupationRate(j) = 1.0;
+			fermi_ = eigVal(npsi-1);
+		}
+		else {
+			throw std::logic_error( "The number of eigenvalues in ev should be larger than nocc" );
+		}
+	}
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::CalculateOccupationRate  ----- 
+
+
+void
+SCF::CalculateEnergy	(  )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::CalculateEnergy");
+#endif
+	Ekin_ = 0.0;
+	DblNumVec&  eigVal         = eigSolPtr_->Ham().EigVal();
+	DblNumVec&  occupationRate = eigSolPtr_->Ham().OccupationRate();
+
+	// Kinetic energy
+	for (Int i=0; i < eigVal.m(); i++) {
+		Ekin_  += eigVal(i) * occupationRate(i);
+	}
+
+	// "Correction" energy 
+	// Hartree and xc part
+	Ecor_ = 0.0;
+	Int  ntot = eigSolPtr_->FFT().domain.NumGridTotal();
+	Real vol  = eigSolPtr_->FFT().domain.Volume();
+	DblNumMat&  density      = eigSolPtr_->Ham().Density();
+  DblNumMat&  vxc          = eigSolPtr_->Ham().Vxc();
+  DblNumVec&  pseudoCharge = eigSolPtr_->Ham().PseudoCharge();
+	DblNumVec&  vhart        = eigSolPtr_->Ham().Vhart();
+	for (Int i=0; i<ntot; i++) {
+		Ecor_ += -vxc(i,VAL) * density(i,VAL) - 
+			0.5 * vhart(i) * ( density(i,VAL) + pseudoCharge(i) );
+	}
+	Ecor_ *= vol/Real(ntot);
+
+	Ecor_ += Exc_;
+
+	// Self energy part
+	Real Es = 0;
+	std::vector<Atom>&  atomList = eigSolPtr_->Ham().AtomList();
+	for(Int a=0; a< atomList.size() ; a++) {
+		Int type = atomList[a].type;
+		Es +=  ptablePtr_->ptemap()[type].params()(PeriodTable::i_Es);
+	}
+	Ecor_ -= Es;
+
+	// Total energy
+	Etot_ = Ekin_ + Ecor_;
+
+	// Helmholtz fre energy
+	if( eigSolPtr_->Ham().NumOccupiedState() == 
+			eigSolPtr_->Ham().NumStateTotal() ){
+		// Zero temperature
+		Efree_ = Etot_;
+	}
+	else{
+		// Finite temperature
+		Efree_ = 0.0;
+		Real fermi = fermi_;
+		Real Tbeta = Tbeta_;
+		for(Int l=0; l< eigVal.m(); l++) {
+			Real eig = eigVal(l);
+			if( eig - fermi >= 0){
+				Efree_ += -1.0/Tbeta*log(1.0+exp(-Tbeta*(eig - fermi))); 
+			}
+			else{
+				Efree_ += (eig - fermi) - 1.0/Tbeta*log(1.0+exp(Tbeta*(eig-fermi)));
+			}
+		}
+		Efree_ += Ecor_ + fermi * eigSolPtr_->Ham().NumOccupiedState(); 
+	}
+
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::CalculateEnergy  ----- 
+
+
+void
+SCF::AndersonMix	( const Int iter )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::AndersonMix");
+#endif
+	DblNumVec vin, vout, vinsave, voutsave;
+
+	Int ntot  = eigSolPtr_->FFT().domain.NumGridTotal();
+
+	vin.Resize(ntot);
+	vout.Resize(ntot);
+	vinsave.Resize(ntot);
+	voutsave.Resize(ntot);
+
+	DblNumVec& vtot = eigSolPtr_->Ham().Vtot();
+
+	for (Int i=0; i<ntot; i++) {
+		vin(i) = vtot(i);
+		vout(i) = vtotNew_(i) - vtot(i);
+	}
+
+	for(Int i = 0; i < ntot; i++){
+	  vinsave(i) = vin(i);
+	  voutsave(i) = vout(i);
+	}
+
+	Int iterused = std::min( iter-1, mixMaxDim_ ); // iter should start from 1
+	Int ipos = iter - 1 - ((iter-2)/ mixMaxDim_ ) * mixMaxDim_;
+
+	// TODO Set verbose level 
+	Print( statusOFS, "Anderson mixing" );
+	Print( statusOFS, "  iterused = ", iterused );
+	Print( statusOFS, "  ipos     = ", ipos );
+
+
+	if( iter > 1 ){
+		for(Int i = 0; i < ntot; i++){
+			dfMat_(i, ipos-1) -= vout(i);
+			dvMat_(i, ipos-1) -= vin(i);
+		}
+
+		// Calculating pseudoinverse
+    
+
+		DblNumVec gammas, S;
+		DblNumMat dftemp;
+		Int rank;
+		// FIXME Magic number
+		Real rcond = 1e-6;
+
+		S.Resize(iterused);
+
+		gammas = vout;
+		dftemp = dfMat_;
+
+		lapack::SVDLeastSquare( ntot, iterused, 1, 
+				dftemp.Data(), ntot, gammas.Data(), ntot,
+        S.Data(), rcond, &rank );
+
+		Print( statusOFS, "  Rank of dfmat = ", rank );
+			
+
+		// Update vin, vout
+
+		blas::Gemv('N', ntot, iterused, -1.0, dvMat_.Data(),
+				ntot, gammas.Data(), 1, 1.0, vin.Data(), 1 );
+		blas::Gemv('N', ntot, iterused, -1.0, dfMat_.Data(),
+				ntot, gammas.Data(), 1, 1.0, vout.Data(), 1 );
+	}
+
+	Int inext = iter - ((iter-1)/ mixMaxDim_) * mixMaxDim_;
+	for (Int i=0; i<ntot; i++) {
+		dfMat_(i, inext-1) = voutsave(i);
+		dvMat_(i, inext-1) = vinsave(i);
+	}
+
+	for (Int i=0; i<ntot; i++) {
+		vtot(i) = vin(i) + mixStepLength_ * vout(i);
+	}
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::AndersonMix  ----- 
+
+
+void
+SCF::KerkerMix	(  )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::KerkerMix");
+#endif
+	// FIXME Magic number here
+	Real mixStepLengthKerker = 0.8; 
+	Int ntot  = eigSolPtr_->FFT().domain.NumGridTotal();
+	DblNumVec& vtot = eigSolPtr_->Ham().Vtot();
+
+  for (Int i=0; i<ntot; i++) {
+		eigSolPtr_->FFT().inputComplexVec(i) = 
+			Complex(vtotNew_(i) - vtot(i), 0.0);
+		// Why?
+		vtot(i) += mixStepLengthKerker * (vtotNew_(i) - vtot(i));
+  }
+  fftw_execute( eigSolPtr_->FFT().forwardPlan );
+	
+	DblNumVec&  gkk = eigSolPtr_->FFT().gkk;
+
+  for(Int i=0; i<ntot; i++) {
+		if( gkk(i) == 0 ){
+			eigSolPtr_->FFT().outputComplexVec(i) = Z_ZERO;
+		}
+		else{
+			// FIXME magic number
+			eigSolPtr_->FFT().outputComplexVec(i) *= 
+				mixStepLengthKerker * ( gkk(i) / (gkk(i)+0.5) - 1.0 );
+		}
+  }
+  fftw_execute( eigSolPtr_->FFT().backwardPlan );
+  
+	// Update vtot
+	for (Int i=0; i<ntot; i++)
+	  vtot(i) += eigSolPtr_->FFT().inputComplexVec(i).real() / ntot;	
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::KerkerMix  ----- 
+
+void
+SCF::PrintState	( const Int iter  )
+{
+#ifndef _RELEASE_
+	PushCallStack("SCF::PrintState");
+#endif
+	{
+		std::ostringstream msg;
+		msg << "SCF iteration # " << iter;
+		PrintBlock( statusOFS, msg.str() );
+	}
+	for(Int i = 0; i < eigSolPtr_->EigVal().m(); i++){
+    Print(statusOFS, 
+				"band#  = ", i, 
+	      "eigval = ", eigSolPtr_->EigVal()(i),
+	      "resval = ", eigSolPtr_->ResVal()(i),
+	      "occupy = ", eigSolPtr_->Ham().OccupationRate()(i));
+	}
+	statusOFS << std::endl;
+	Print(statusOFS, "Etot              = ",  Etot_, "[au]");
+	Print(statusOFS, "Efree             = ",  Efree_, "[au]");
+	Print(statusOFS, "Ekin              = ",  Ekin_, "[au]");
+	Print(statusOFS, "Ecor              = ",  Ecor_, "[au]");
+	Print(statusOFS, "Exc               = ",  Exc_, "[au]"); 
+	Print(statusOFS, "Fermi             = ",  fermi_, "[au]");
+	Print(statusOFS, "Total charge      = ",  totalCharge_, "[au]");
+	Print(statusOFS, "norm(vout-vin)/norm(vin) = ", scfNorm_ );
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+} 		// -----  end of method SCF::PrintState  ----- 
+
+
+void
+PrintInitialState ( const esdf::ESDFInputParam& esdfParam )
+{
+#ifndef _RELEASE_
+	PushCallStack("PrintInitialState");
+#endif
+	PrintBlock(statusOFS, "Basic information");
+
+	Print(statusOFS, "Super cell        = ",  esdfParam.domain.length );
+	Print(statusOFS, "Grid size         = ",  esdfParam.domain.numGrid ); 
+	Print(statusOFS, "Mixing dimension  = ",  esdfParam.mixMaxDim );
+	Print(statusOFS, "Mixing type       = ",  esdfParam.mixType );
+	Print(statusOFS, "Mixing Steplength = ",  esdfParam.mixStepLength);
+	Print(statusOFS, "SCF Tolerence     = ",  esdfParam.scfTolerance);
+	Print(statusOFS, "SCF MaxIter       = ",  esdfParam.scfMaxIter);
+	Print(statusOFS, "Eig Tolerence     = ",  esdfParam.eigTolerance);
+	Print(statusOFS, "Eig MaxIter       = ",  esdfParam.eigMaxIter);
+
+	Print(statusOFS, "RestartDensity    = ",  esdfParam.isRestartDensity);
+	Print(statusOFS, "RestartWfn        = ",  esdfParam.isRestartWfn);
+	Print(statusOFS, "OutputDensity     = ",  esdfParam.isOutputDensity);
+	Print(statusOFS, "OutputWfn         = ",  esdfParam.isOutputWfn);
+
+	Print(statusOFS, "Temperature       = ",  au2K / esdfParam.Tbeta, "[K]");
+	Print(statusOFS, "Extra states      = ",  esdfParam.numExtraState );
+	Print(statusOFS, "PeriodTable File  = ",  esdfParam.periodTableFile );
+	Print(statusOFS, "Pseudo Type       = ",  esdfParam.pseudoType );
+	Print(statusOFS, "PW Solver         = ",  esdfParam.PWSolver );
+	Print(statusOFS, "XC Type           = ",  esdfParam.XCType );
+
+	PrintBlock(statusOFS, "Atom Type and Coordinates");
+
+	const std::vector<Atom>&  atomList = esdfParam.atomList;
+	for(Int i=0; i < atomList.size(); i++) {
+		Print(statusOFS, "Type = ", atomList[i].type, "Position  = ", atomList[i].pos);
+	}
+
+#ifndef _RELEASE_
+	PopCallStack();
+#endif
+
+	return ;
+}		// -----  end of function PrintInitialState  ----- 
+
+} // namespace dgdft
