3f6cf5b6d5986a0b2cbfd1e81ea7ee975f1e33f7
linlin <linlin@math.berkeley.edu>
2015-04-24 14:33:10 -0700

    Add the test routine for the new LGL mesh/weight generation function.

---
+++ tests/ex39.cpp
@@ -0,0 +1,107 @@
+/*
+	 Copyright (c) 2012 The Regents of the University of California,
+	 through Lawrence Berkeley National Laboratory.  
+
+   Author: Lin Lin
+	 
+   This file is part of DGDFT. All rights reserved.
+
+	 Redistribution and use in source and binary forms, with or without
+	 modification, are permitted provided that the following conditions are met:
+
+	 (1) Redistributions of source code must retain the above copyright notice, this
+	 list of conditions and the following disclaimer.
+	 (2) Redistributions in binary form must reproduce the above copyright notice,
+	 this list of conditions and the following disclaimer in the documentation
+	 and/or other materials provided with the distribution.
+	 (3) Neither the name of the University of California, Lawrence Berkeley
+	 National Laboratory, U.S. Dept. of Energy nor the names of its contributors may
+	 be used to endorse or promote products derived from this software without
+	 specific prior written permission.
+
+	 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	 DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	 ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+	 ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	 You are under no obligation whatsoever to provide any bug fixes, patches, or
+	 upgrades to the features, functionality or performance of the source code
+	 ("Enhancements") to anyone; however, if you choose to make your Enhancements
+	 available either publicly, or directly to Lawrence Berkeley National
+	 Laboratory, without imposing a separate written license agreement for such
+	 Enhancements, then you hereby grant the following license: a non-exclusive,
+	 royalty-free perpetual license to install, use, modify, prepare derivative
+	 works, incorporate into other computer software, distribute, and sublicense
+	 such enhancements or derivative works thereof, in binary and source code form.
+*/
+/// @file ex39.cpp
+/// @brief Testing the LGL mesh generator and the simplified LGL mesh
+/// generator.
+/// @date 2015-04-24
+#include "dgdft.hpp"
+
+using namespace dgdft;
+using namespace std;
+using namespace dgdft::esdf;
+
+void Usage(){
+  std::cout 
+		<< "ex39 " << std::endl;
+}
+
+
+int main(int argc, char **argv) 
+{
+	MPI_Init(&argc, &argv);
+	int mpirank, mpisize;
+	MPI_Comm_rank( MPI_COMM_WORLD, &mpirank );
+	MPI_Comm_size( MPI_COMM_WORLD, &mpisize );
+	Real timeSta, timeEnd;
+
+	if( mpirank == 0 )
+		Usage();
+
+
+	try
+	{
+    DblNumVec x, w;
+    DblNumVec x1, w1;
+    DblNumMat P, D;
+    Int N = 10;
+    std::cout << "Old code." << std::endl;
+    GenerateLGL( x, w, P, D, N );
+    std::cout << "x = " << x << std::endl;
+    std::cout << "w = " << w << std::endl;
+    std::cout << "New code." << std::endl;
+    GenerateLGLMeshWeightOnly( x1, w1, N );
+    std::cout << "x1 = " << x1 << std::endl;
+    std::cout << "w1 = " << w1 << std::endl;
+    std::cout << std::endl;
+    Real errx = 0.0;
+    Real errw = 0.0;
+    for(Int i = 0; i < N; i++ ){
+      errx = (x1[i] - x[i])*(x1[i] - x[i]);
+      errw = (w1[i] - w[i])*(w1[i] - w[i]);
+    }
+    std::cout << "norm(x1 - x, 2) = " << std::sqrt(errx) << std::endl;
+    std::cout << "norm(w1 - w, 2) = " << std::sqrt(errw) << std::endl;
+	}
+	catch( std::exception& e )
+	{
+		std::cerr << " caught exception with message: "
+			<< e.what() << std::endl;
+#ifndef _RELEASE_
+		DumpCallStack();
+#endif
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
