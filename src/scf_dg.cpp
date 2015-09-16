ec662ff8e2100d2147f3bed0aca57e8a21ef3eb5
linlin <linlin@math.berkeley.edu>
2015-09-15 21:42:11 -0700

    Fix the problem for restarting density and ALBs.
    This is due to an incompatible output format that were updated a while
    ago.
    
    The soft pseudopotential HGH is also updated.

--- src/scf_dg.cpp
+++ src/scf_dg.cpp
@@ -421,6 +421,12 @@ SCFDG::Setup	(
           for( Int i = 0; i < numElem_[0]; i++ ){
             Index3 key( i, j, k );
             if( elemPrtn_.Owner( key ) == (mpirank / dmRow_) ){
+              std::vector<DblNumVec> gridpos(DIM);
+              
+              // Dummy variables and not used
+              for( Int d = 0; d < DIM; d++ ){
+                deserialize( gridpos[d], rhoStream, NO_MASK );
+              }
 
               Index3 keyRead;
               deserialize( keyRead, rhoStream, NO_MASK );
@@ -553,10 +559,10 @@ SCFDG::Setup	(
             DblNumTns& wavefun = psi.Wavefun();
             DblNumTns  wavefunRead;
 
-//            std::vector<DblNumVec> gridpos(DIM);
-//            for( Int d = 0; d < DIM; d++ ){
-//              deserialize( gridpos[d], wfnStream, NO_MASK );
-//            }
+            std::vector<DblNumVec> gridpos(DIM);
+            for( Int d = 0; d < DIM; d++ ){
+              deserialize( gridpos[d], wfnStream, NO_MASK );
+            }
 
             Index3 keyRead;
             deserialize( keyRead, wfnStream, NO_MASK );
