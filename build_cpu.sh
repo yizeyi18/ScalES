3620f5e8caf06e48566f03021cba3bbd31bbb76d
Wei Hu <whuustc@gmail.com>
2021-10-04 20:22:53 -0700

    compile pwdft on cori

--- build_cpu.sh
+++ build_cpu.sh
@@ -4,6 +4,6 @@
 # new with cmake
 BDIR=build_scales_cpu
 rm -rf ${BDIR}
-/home/linlin/Software/cmake-3.19.2-Linux-x86_64/bin/cmake -H. -B${BDIR} -DCMAKE_TOOLCHAIN_FILE=/home/linlin/Projects/scales/config/toolchain.cmake.linux \
+/global/common/sw/cray/cnl7/haswell/cmake/3.18.2/bin/cmake -H. -B${BDIR} -DCMAKE_TOOLCHAIN_FILE=/global/homes/w/weihu/project_weihu/jielanli/ScalES/scales_test/config/toolchain.cmake.cori \
   -DCMAKE_BUILD_TYPE=Release -DScalES_ENABLE_CUDA=OFF -DScalES_ENABLE_OPENMP=OFF -Duse_openmp=OFF
 make -C ${BDIR} -j 12
