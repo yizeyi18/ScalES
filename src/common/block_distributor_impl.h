0b42b9e1395bd6a6ba91159e10476482f6cdfc9b
yizeyi18 <18586016708@163.com>
2023-11-15 16:42:47 +0800

    给block_distributor_impl加了注释。里面没有什么算法层面上的实现。

--- src/common/block_distributor_impl.h
+++ src/common/block_distributor_impl.h
@@ -21,18 +21,18 @@ protected:
   Int      comm_rank_;
   Int      comm_size_;
 
-  Int M_;
+  Int M_;                                                      //M/N是做什么的？待补
   Int N_;
   Int MBlock_;
   Int NBlock_;
-  Int MRem_;
+  Int MRem_;                                                   //Rem==Remain，block填不上的部分
   Int NRem_;
   Int MLocal_;
   Int NLocal_;
 
 public:
 
-  BlockDistributorImpl( MPI_Comm comm, Int M, Int N ) :
+  BlockDistributorImpl( MPI_Comm comm, Int M, Int N ) :        //构造函数
     comm_(comm), M_(M), N_(N) {
 
     MPI_Comm_rank( comm_, &comm_rank_); 
@@ -43,10 +43,10 @@ public:
     MRem_   = M % comm_size_;
     NRem_   = N % comm_size_;
 
-    MLocal_ = MBlock_ + !!(comm_rank_ < ( MRem_ ));
-    NLocal_ = NBlock_ + !!(comm_rank_ < ( NRem_ ));
+    MLocal_ = MBlock_ + !!(comm_rank_ < ( MRem_ ));            //!!不是单独的算符，它只是非非
+    NLocal_ = NBlock_ + !!(comm_rank_ < ( NRem_ ));            //把真值搞成1用的，叠了一个bool->int的隐式转换，感觉直接(int)强转也行？
 
-  };
+  };//BlockDistributorImpl()
   
 
   inline auto comm()      const noexcept { return comm_;      }
@@ -58,19 +58,20 @@ public:
   inline auto MLocal() const noexcept { return MLocal_; }
   inline auto NLocal() const noexcept { return NLocal_; }
 
-  virtual void redistribute_row_to_col( const NumMat<T>& row_data, NumMat<T>& col_data ) = 0;
-  virtual void redistribute_col_to_row( const NumMat<T>& col_data, NumMat<T>& row_data ) = 0;
+  virtual void redistribute_row_to_col( const NumMat<T>& row_data, NumMat<T>& col_data ) = 0; //实现见host_optpack_block_distributor.h
+  virtual void redistribute_col_to_row( const NumMat<T>& col_data, NumMat<T>& row_data ) = 0; //与    host_generic_block_distributor.h
 
-};
+};//class BlockDistributorImpl
 
-}
+}//namespace detail
 
+//接下来是BlockDistributor的实现
 
 template <typename T>
 BlockDistributor<T>::BlockDistributor( 
   std::unique_ptr<detail::BlockDistributorImpl<T>>&& impl 
 ) :
-  impl_(std::move(impl)) { }
+  impl_(std::move(impl)) { }                                   //unique_ptr实战！构造函数其一。
 
 template <typename T>
 BlockDistributor<T>::BlockDistributor() noexcept : impl_(nullptr) { };
@@ -78,26 +79,27 @@ BlockDistributor<T>::BlockDistributor() noexcept : impl_(nullptr) { };
 //template <typename T>
 //BlockDistributor<T>::BlockDistributor( MPI_Comm comm, Int M, Int N ) :
 //  BlockDistributor( detail::make_default_host_<T>( comm, M, N ) ) { }
-  
+// 看样子曾经没有那个BlockDistributor_impl类？
+
 template <typename T>
-BlockDistributor<T>::~BlockDistributor() noexcept = default;
+BlockDistributor<T>::~BlockDistributor() noexcept = default;   //构造函数其二
 
 
 template <typename T>
 BlockDistributor<T>& BlockDistributor<T>::operator=( BlockDistributor&& other ) noexcept {
   impl_ = std::move(other.impl_);
   return (*this);
-}
+}                                                              //赋值
 
 template <typename T>
 BlockDistributor<T>::BlockDistributor( BlockDistributor&& other ) noexcept :
-  BlockDistributor( std::move(other.impl_) ) { }
+  BlockDistributor( std::move(other.impl_) ) { }               //构造函数其三
 
 
 template <typename T>
 void BlockDistributor<T>::redistribute_row_to_col( const NumMat<T>& row_data, 
                                                          NumMat<T>& col_data ) {
-  if( impl_ )
+  if( impl_ )//等价于impl_ != nullptr？
     impl_->redistribute_row_to_col( row_data, col_data );
   else throw std::runtime_error("BlockDistributor Has Not Been Initialized");
 }
